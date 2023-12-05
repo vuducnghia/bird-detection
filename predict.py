a7553e46a3db47c79ebd67e89054b4120a3ffd72
James Vu <vuducnghia1996@gmail.com>
2023-12-05 09:22:52 +0700

    detect from camera

--- predict.py
+++ predict.py
@@ -1,16 +1,38 @@
 from ultralytics import YOLO
 from PIL import Image
+import cv2
 
 model = YOLO("results/weights/best.pt")
 
-results = model.predict(source="test/images/000058.png", imgsz=640, conf=0.45)
-for r in results:
-    print(r.boxes.xywh)
-    print(r.boxes.conf)
-    print(r.speed)
+# detect from a image
+# results = model.predict(source="test/images/000058.png", imgsz=640, conf=0.45)
+# for r in results:
+#     print(r.boxes.xywh)
+#     print(r.boxes.conf)
+#     print(r.speed)
+#
+#     # plot result:
+#     im_array = r.plot()  # plot a BGR numpy array of predictions
+#     im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
+#     im.show()
+#     im.save('results.jpg')  # save image
 
-    # plot result:
-    im_array = r.plot()  # plot a BGR numpy array of predictions
-    im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
-    im.show()
-    im.save('results.jpg')  # save image
+
+# detect from camera
+cam = cv2.VideoCapture(0)
+cv2.namedWindow("video")
+img_counter = 0
+while True:
+    ret, frame = cam.read()
+    img_counter += 1
+    # print(img_counter)
+    if img_counter % 3 == 0:
+        continue
+    results = model(frame, imgsz=640, conf=0.55)
+    # print(results)
+    for r in results:
+        cv2.imshow("test", r.plot())
+
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
